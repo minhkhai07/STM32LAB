46b1270c65a3ec36e95026e51c4a78f66856516e
ADMIN\Admin <khai.to0707@hcmut.edu.vn>
2024-09-25 12:37:45 +0700

    Init EX7

--- VXLLAB1/Core/Src/main.c
+++ VXLLAB1/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -60,6 +61,21 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
+void clearAllClock () {
+		HAL_GPIO_WritePin (GPIOA , LED_RED_Pin ,GPIO_PIN_SET);
+		HAL_GPIO_WritePin (GPIOA , LED_REDA5_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA6_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA7_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA8_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA9_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA10_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA11_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA12_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA13_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA14_Pin ,GPIO_PIN_SET );
+		HAL_GPIO_WritePin (GPIOA , LED_REDA15_Pin ,GPIO_PIN_SET );
+
+	}
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -83,6 +99,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -131,6 +148,36 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_REDA5_Pin|LED_REDA6_Pin|LED_REDA7_Pin
+                          |LED_REDA8_Pin|LED_REDA9_Pin|LED_REDA10_Pin|LED_REDA11_Pin
+                          |LED_REDA12_Pin|LED_REDA13_Pin|LED_REDA14_Pin|LED_REDA15_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_RED_Pin LED_REDA5_Pin LED_REDA6_Pin LED_REDA7_Pin
+                           LED_REDA8_Pin LED_REDA9_Pin LED_REDA10_Pin LED_REDA11_Pin
+                           LED_REDA12_Pin LED_REDA13_Pin LED_REDA14_Pin LED_REDA15_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_REDA5_Pin|LED_REDA6_Pin|LED_REDA7_Pin
+                          |LED_REDA8_Pin|LED_REDA9_Pin|LED_REDA10_Pin|LED_REDA11_Pin
+                          |LED_REDA12_Pin|LED_REDA13_Pin|LED_REDA14_Pin|LED_REDA15_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
